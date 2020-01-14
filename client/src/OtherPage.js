c6cba7da5497063d5d63c4f1e0926d7e8d2f16e0
Salman Ahmad <salman.ahmad@nxb.com.pk>
2020-01-14 17:01:41 +0500

    Init Commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      Content
+      <Link to="/">Go Back</Link>
+    </div>
+  )
+};
